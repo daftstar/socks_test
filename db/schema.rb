44595d83184670d5ca4d9cee2b4c832388e0ed2b
Nik Daftary <nikdaftary@gmail.com>
2013-10-18 10:18:34 -0500

    successful swap to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20131018145537) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "socks", force: true do |t|
     t.string   "color"
     t.string   "foot"
